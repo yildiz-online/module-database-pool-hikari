1c46e29a90cf61ea352e070a96a0cb7d5a5f0747
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-05-11 12:24:20 +0200

    [WIP] Vault.

--- deploy-maven-central.sh
+++ deploy-maven-central.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "<<<<<<<<<<<<<<<<<<<<<"
-echo $GPG_KEY
+echo $GPG_PWD
 echo ">>>>>>>>>>>>>>>>>>>>>"
 
 SECRETS=$(curl -sS -H "X-Vault-Token: $VAULT_TOKEN" -X GET https://vault.yildiz-games.be/v1/kv/yildiz-engine)
