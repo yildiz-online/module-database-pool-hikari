1771360d7dad0114bce8c12ac1367f9220f6f341
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-05-11 11:16:14 +0200

    [WIP] Vault.

--- deploy-maven-central.sh
+++ deploy-maven-central.sh
@@ -5,7 +5,9 @@ SECRETS=$(curl -sS -H "X-Vault-Token: $VAULT_TOKEN" -X GET https://vault.yildiz-
 GH_TOKEN=$(echo $SECRETS | jq -r '.data.GH_TOKEN')
 OPENSSL_PWD=$(echo $SECRETS | jq -r '.data.OPENSSL_PWD')
 GPG_KEY=$(echo $SECRETS | jq -r '.data.GPG_KEY')
+#GPG_PWD=$(echo $SECRETS | jq -r '.data.GPG_PWD')
 #OSSRH_USER_TOKEN=$(echo $SECRETS | jq -r '.data.OSSRH_USER_TOKEN')
+OSSRH_PWD_TOKEN=$(echo $SECRETS | jq -r '.data.OSSRH_PWD_TOKEN')
 SONAR=$(echo $SECRETS | jq -r '.data.SONAR')
 SONAR_ORGANIZATION=$(echo $SECRETS | jq -r '.data.SONAR_ORGANIZATION')
 
